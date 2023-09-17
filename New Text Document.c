23bb2464b1d60c0ac5d20fbb1c95aa911d06c176
abul253698 <145246187+abul253698@users.noreply.github.com>
2023-09-17 11:54:47 +0600

    Add files via upload

---
+++ New Text Document.c
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+void main() {
+    
+    int pi,r,h,a,b;
+     pi =3.1416;r =5; h = 6; a= 2; b=3;
+    
+    int area = pi*r*r*h;
+    int sum = (a+b)*(a+b);
+    
+    printf("The value is %d", area);
+    printf("\nThe value is %d", sum);
+
+    
+}
